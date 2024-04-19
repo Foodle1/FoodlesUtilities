349dcdbf58eda9d93c5625ebcfa47b414d093076
Jack <jack.waldron2001@gmail.com>
2024-04-19 23:14:48 +1000

    Change ScreenshotGrabber.cs save file name to current scene name + current time

--- Editor/ScreenshotGrabber.cs
+++ Editor/ScreenshotGrabber.cs
@@ -1,14 +1,20 @@
+using System;
 using UnityEditor;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FoodlesUtilities.Editor
 {
     public static class ScreenshotGrabber
     {
         [MenuItem("Screenshot/Grab")]
+        [Shortcut("Screenshot", KeyCode.F6, ShortcutModifiers.Shift)]
         public static void Grab()
         {
-            ScreenCapture.CaptureScreenshot("Screenshot.png", 1);
+            var localTime = DateTime.Now.ToLocalTime();
+            var formattedString = $"{SceneManager.GetActiveScene().name}-{localTime:yyyy-MM-dd}-{localTime:HH-mm-ss}.png";
+            ScreenCapture.CaptureScreenshot(formattedString, 1);
         }
     }
 }
