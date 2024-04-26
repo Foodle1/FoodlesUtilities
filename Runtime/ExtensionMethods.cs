3472fc094ce9becb9223af96cc2a0084e4efdc9c
Jack <jack.waldron2001@gmail.com>
2024-04-26 14:29:45 +1000

    Add AddSpacesToString to ExtensionMethods.cs

--- Runtime/ExtensionMethods.cs
+++ Runtime/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -41,6 +42,24 @@ namespace FoodlesUtilities
                 3 => number + "rd",
                 _ => number + "th"
             };
+        }
+        
+        public static string AddSpacesToString(string text, bool preserveAcronyms = true)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+            var newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (var i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                    if ((text[i - 1] != ' ' && !char.IsUpper(text[i - 1])) ||
+                        (preserveAcronyms && char.IsUpper(text[i - 1]) && 
+                         i < text.Length - 1 && !char.IsUpper(text[i + 1])))
+                        newText.Append(' ');
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
         }
 
         #endregion
